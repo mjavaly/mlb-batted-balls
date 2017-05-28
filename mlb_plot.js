93bfd2e936d755b0cf7edfc645d0a86ffb7c5f88
Matt <javalym@carleton.edu>
2017-05-27 22:00:09 -0500

    Added search mechanism for pitchers, batters and stadiums, although choosing a new one does not actually do anything yet.

--- mlb_plot.js
+++ mlb_plot.js
@@ -1,24 +1,51 @@
 // Part of a brief D3 tutorial.
 // Upon completion, will display an interactive scatterplot showing relationship between
-//   different values associated with the top 100 words in Shakespeare's First Folio
+//   different values associated with the top 100 words in Shakespeare"s First Folio
 // CS 314, Spring 2017
 // Eric Alexander
 
 // First, we will create some constants to define non-data-related parts of the visualization
-w = 1000;           // Width of our visualization
+w = 1050;           // Width of our visualization
 h = 1000;           // Height of our visualization
 margin = 120;        // Margin around visualization
-vals = ['x','y']
+vals = ["x","y"]
 xVal = vals[0]
 yVal = vals[1]
+teamStadiums = {"Red Sox":"Fenway Park",
+				"Angels":"Angel Stadium",
+				"Tigers":"Comerica Park",
+				"Rangers":"Globe Life Park",
+				"White Sox":"Guaranteed Rate Field",
+				"Royals":"Kauffman Stadium",
+				"Astros":"Minute Maid Park",
+				"Athletics":"Oakland Alameda Coliseum",
+				"Orioles":"Oriole Park",
+				"Indians":"Progressive Field",
+				"Blue Jays":"Rogers Centre",
+				"Mariners":"Safeco Field",
+				"Twins":"Target Field",
+				"Rays":"Tropicana Field",
+				"Yankees":"Yankee Stadium",
+				"Giants":"AT&T Park",
+				"Cardinals":"Busch Stadium",
+				"Diamondbacks":"Chase Field",
+				"Mets":"Citi Field",
+				"Phillies":"Citizens Bank Park",
+				"Rockies":"Coors Field",
+				"Dodgers":"Dodger Stadium",
+				"Reds":"Great American Ball Park",
+				"Marlins":"Marlins Park",
+				"Brewers":"Miller Park",
+				"Nationals":"Nationals Park",
+				"Padres":"Petco Park",
+				"Pirates":"PNC Park",
+				"Braves":"Turner Field",
+				"Cubs":"Wrigley Field"}
+d3.json("bbs-2016.json", function(jsonData) {
 
-
-function updatePoints(stadium, batter, pitcher) {
-	// Next, we will load in our CSV of data
-	d3.json('bbs-2016.json', function(jsonData) {
+	function updatePoints(stadium, batter, pitcher) {
 
 		data = jsonData[stadium];
-
 		// This will define scales that convert values
 		// from our data domain into screen coordinates.
 		xScale = d3.scale.linear()
@@ -31,9 +58,9 @@ function updatePoints(stadium, batter, pitcher) {
 					.range([h - margin, margin]); // Notice this is backwards!
 
 		// Next, we will create an SVG element to contain our visualization.
-		var pointPlot = d3.select('#pointsSVG').append('svg:svg')
-			.attr('width', w+600)
-			.attr('height', h-200)
+		var pointPlot = d3.select("#pointsSVG").append("svg:svg")
+			.attr("width", w+600)
+			.attr("height", h-200)
 
 		var x_hp = 125;
 		var y_hp = 43;
@@ -74,31 +101,65 @@ function updatePoints(stadium, batter, pitcher) {
 			.attr("stroke-width", 2)
 			.attr("stroke", "black");
 
-		var circle = pointPlot.selectAll('circle')
+		var circle = pointPlot.selectAll("circle")
 				.data(data);
 			circle.enter()
-				.append('svg:circle')
+				.append("svg:circle")
 				.filter(function(d) {
 					var p = (pitcher == "*") ? true : d["pitcher_name"] == pitcher;
 					var b = (batter == "*") ? true : d["batter_name"] == batter;
 					return p && b;
 				})
-				.attr('cx', function(d) { return xScale(d['x']); })
-				.attr('cy', function(d) { return yScale(250-d['y']); })
-				.style('fill', function(d) {
-					if (d['type'] == 'O') {
-						return 'red';
+				.attr("cx", function(d) { return xScale(d["x"]); })
+				.attr("cy", function(d) { return yScale(250-d["y"]); })
+				.style("fill", function(d) {
+					if (d["type"] == "O") {
+						return "red";
 					}
-					else if (d['type'] == 'H') {
-						return 'blue';
+					else if (d["type"] == "H") {
+						return "blue";
 					}
 					else {
-						return 'black';
+						return "black";
 					}
 				})
-				.attr('r', 3)
-				.on('mouseover', function(d) { console.log(d); })
+				.attr("r", 3)
+				.on("mouseover", function(d) { console.log(d); })
+	}
+	
+	$(function() {
+		var batters = new Set();
+		var pitchers = new Set();
+		for (park in jsonData) {
+			for (var i = 0; i < jsonData[park].length; i++) {
+				var hit = jsonData[park][i];
+				batters.add(hit["batter_name"]);
+				pitchers.add(hit["pitcher_name"]);
+			}
+		}
+		let batterList = Array.from(batters);
+		let pitcherList = Array.from(pitchers);
+		$("#batters").autocomplete({ source: batterList });
+		$("#pitchers").autocomplete({ source: pitcherList });
 	});
-}
 
-updatePoints('Twins','Brian Dozier', '*');
+	updatePoints("Twins","Brian Dozier", "*");
+});
+
+var park = d3.select("#ballparks");
+console.log(park.data);
+
+park.on("input", function() {
+	var newData = d3.select(this);
+	console.log(newData);
+	// updatePoints(newData);
+});
+
+$(function() {
+	var bps = [];
+	for (team in teamStadiums) {
+		bps.push(team + " (" + teamStadiums[team] + ")");
+	}
+	$("#ballparks").autocomplete({ source: bps });
+});
+
